5a010581692c5cfbfc36e3ad84bbb00bc63bd029
Juanpe <juanpedrogomezespinosa@gmail.com>
2025-10-03 12:55:32 +0200

    corrección tarea

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -17,6 +17,48 @@ describe("Gestión de tareas con POM", () => {
     const taskName = "Eliminar esta tarea";
     todoPage.addTodo(taskName);
     todoPage.deleteTodo(taskName);
-    todoPage.elements.todoItems().should("not.exist");
+    todoPage.validateTodoNotExists(taskName);
+  });
+
+  it("Debe permitir editar una tarea", () => {
+    const oldName = "Aprender Cypress";
+    const newName = "Aprender Cypress + Cucumber";
+    todoPage.addTodo(oldName);
+    todoPage.editTodo(oldName, newName);
+  });
+
+  it("Debe permitir marcar una tarea como completada", () => {
+    const taskName = "Estudiar JavaScript";
+    todoPage.addTodo(taskName);
+    todoPage.markCompleted(taskName);
+    todoPage.validateCompleted(taskName);
+  });
+
+  it("Debe permitir desmarcar una tarea completada", () => {
+    const taskName = "Hacer ejercicio";
+    todoPage.addTodo(taskName);
+    todoPage.markCompleted(taskName);
+    todoPage.unmarkCompleted(taskName);
+    todoPage.validateNotCompleted(taskName);
+  });
+
+  it("Debe permitir filtrar tareas completadas y activas", () => {
+    const task1 = "Tarea 1";
+    const task2 = "Tarea 2";
+    todoPage.addTodo(task1);
+    todoPage.addTodo(task2);
+    todoPage.markCompleted(task1);
+
+    todoPage.filterCompleted();
+    todoPage.validateTodoExists(task1);
+    todoPage.validateTodoNotExists(task2);
+
+    todoPage.filterActive();
+    todoPage.validateTodoExists(task2);
+    todoPage.validateTodoNotExists(task1);
+
+    todoPage.filterAll();
+    todoPage.validateTodoExists(task1);
+    todoPage.validateTodoExists(task2);
   });
 });
--- cypress/support/pages/todoPage.js
+++ cypress/support/pages/todoPage.js
@@ -1,25 +1,75 @@
 class TodoPage {
-  // Selectores
   elements = {
     inputNewTodo: () => cy.get("input.new-todo"),
     todoItemLabel: (itemText) => cy.contains("label", itemText),
     todoItems: () => cy.get(".todo-list li"),
     deleteButton: (itemText) =>
       this.elements.todoItemLabel(itemText).parent().find("button.destroy"),
+    toggleButton: (itemText) =>
+      this.elements.todoItemLabel(itemText).parent().find("input.toggle"),
+    filterActive: () => cy.contains("a", "Active"),
+    filterCompleted: () => cy.contains("a", "Completed"),
+    filterAll: () => cy.contains("a", "All"),
   };
 
-  // Métodos
   addTodo(taskName) {
     this.elements.inputNewTodo().type(`${taskName}{enter}`);
   }
 
   deleteTodo(taskName) {
-    // Hacer hover implícito y click forzado
     this.elements.deleteButton(taskName).click({ force: true });
   }
 
   validateTodoExists(taskName) {
     this.elements.todoItemLabel(taskName).should("be.visible");
+  }
+
+  validateTodoNotExists(taskName) {
+    this.elements.todoItemLabel(taskName).should("not.exist");
+  }
+
+  editTodo(oldName, newName) {
+    cy.contains(".todo-list li", oldName).dblclick();
+    cy.get("input.new-todo")
+      .filter(":visible")
+      .first()
+      .type("{selectall}{backspace}")
+      .type(`${newName}{enter}`);
+    this.validateTodoExists(newName);
+  }
+
+  markCompleted(taskName) {
+    this.elements.toggleButton(taskName).check();
+  }
+
+  unmarkCompleted(taskName) {
+    this.elements.toggleButton(taskName).uncheck();
+  }
+
+  validateCompleted(taskName) {
+    this.elements
+      .todoItemLabel(taskName)
+      .closest("li")
+      .should("have.class", "completed");
+  }
+
+  validateNotCompleted(taskName) {
+    this.elements
+      .todoItemLabel(taskName)
+      .closest("li")
+      .should("not.have.class", "completed");
+  }
+
+  filterActive() {
+    this.elements.filterActive().click();
+  }
+
+  filterCompleted() {
+    this.elements.filterCompleted().click();
+  }
+
+  filterAll() {
+    this.elements.filterAll().click();
   }
 }
 
